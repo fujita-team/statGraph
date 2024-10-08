ac958d29cb8a9508489bde755e263f3f3f4e7b0e
lesserfish <lesserfish@pm.me>
2024-10-08 19:16:29 +0900

    Updated the tests
    Hopefully, things  will work!

---
+++ test.r
@@ -0,0 +1,13 @@
+#!/usr/bin/env Rscript
+
+setwd("./statGraph")
+
+# Use system packages instead of RENV. Makes github workflow faster as it does not need to install R dependencies again.
+if(nzchar(Sys.getenv("DISABLE_RENV"))) {
+    renv::deactivate()
+} else {
+    renv::install()
+    renv::load()
+}
+
+devtools::test(stop_on_failure = FALSE)
