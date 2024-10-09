57168b741040b820601607258e7cbdae9cee9669
lesserfish <lesserfish@pm.me>
2024-10-09 16:54:06 +0900

    Minor changes to the tests as they had a little bug
    Just made sure that everything was for undirected graphs.

--- statGraph/tests/testthat/test-graph.param.estimator.R
+++ statGraph/tests/testthat/test-graph.param.estimator.R
@@ -24,7 +24,7 @@ RunTest( {
         return(igraph::sample_gnp(n, p))
     }
     G <- model(100, 0.3)
-    result <- graph.param.estimator(G, model,  seq(0.2, 0.8, 0.01), search="ternary")
+    result <- suppressWarnings(graph.param.estimator(G, model,  seq(0.2, 0.8, 0.01), search="ternary"))
     test_that("Ternary search is working",
               {
                 expect_equal(result$param, 0.3, tolerance = tolerance)
