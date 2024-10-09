f3deae767cb858f84d2ff94b3873e5de43d04037
lesserfish <lesserfish@pm.me>
2024-10-09 17:46:12 +0900

    More tests, for GIC and graph.dist

--- statGraph/tests/testthat/test-GIC.R
+++ statGraph/tests/testthat/test-GIC.R
@@ -3,21 +3,17 @@
 # Run 'Rscript tests.R' to perform tests or 'Rscript build.r' to build the project.
 # For Documentation on how to create tests, please refer to: https://cran.r-project.org/web/packages/testthat/testthat.pdf
 
-# RunTest("GIC", {
-#             acceptable_error <- 0.1
-#             G <- igraph::sample_gnp(n=50, p=0.5)
-#
-#             result1 <- GIC(G, "ER", 0.5)
-#             test_that("GIC", {
-#                 expect_lt(result1$value, acceptable_error)
-#             })
-# })
-# RunTest("GIC", {
-#             model <- function(n, p){
-#                 return(igraph::sample_gnp(n, p))
-#             }
-#             result2 <- GIC(G, model, 0.5)
-#             test_that("GIC", {
-#                 expect_lt(result1$value, acceptable_error)
-#             })
-# })
+RunTest({
+            acceptable_error <- 0.1
+            G <- igraph::sample_gnp(n=50, p=0.5)
+            result1 <- GIC(G, "ER", 0.5)
+            test_that("GIC", {
+                expect_lt(result1$value, acceptable_error)
+            })
+})
+RunTest({
+            result2 <- GIC(G, igraph::sample_gnp, 0.5)
+            test_that("GIC", {
+                expect_lt(result1$value, acceptable_error)
+            })
+})
--- statGraph/tests/testthat/test-graph.dist.R
+++ statGraph/tests/testthat/test-graph.dist.R
@@ -13,3 +13,36 @@ RunTest( {
     expect_true(identical(d1, d2))
   })
 })
+
+RunTest( {
+  A <- list()
+  for(i in 1:10){
+    p <- sample(seq(0.1, 0.9, 0.1), 1)
+    A[[i]] <- igraph::sample_gnp(50, p)
+  }
+
+  # L1 is symmetric, so d1 should be equal to d2
+  d1 <- statGraph::graph.dist(A, dist="L1", symmetric = FALSE)
+  d2 <- statGraph::graph.dist(A, dist="L1", symmetric = TRUE)
+
+  test_that("graph.dist is working well", {
+    expect_true(identical(d1, d2))
+  })
+})
+
+
+RunTest( {
+  A <- list()
+  for(i in 1:10){
+    p <- sample(seq(0.1, 0.9, 0.1), 1)
+    A[[i]] <- igraph::sample_gnp(50, p)
+  }
+
+  # L2 is symmetric, so d1 should be equal to d2
+  d1 <- statGraph::graph.dist(A, dist="L2", symmetric = FALSE)
+  d2 <- statGraph::graph.dist(A, dist="L2", symmetric = TRUE)
+
+  test_that("graph.dist is working well", {
+    expect_true(identical(d1, d2))
+  })
+})
