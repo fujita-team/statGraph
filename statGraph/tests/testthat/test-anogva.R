4103fbfe7e801188ef47d956ee7811e3ec289c00
lesserfish <lesserfish@pm.me>
2024-10-16 15:07:31 +0900

    Anogva is working!

--- statGraph/tests/testthat/test-anogva.R
+++ statGraph/tests/testthat/test-anogva.R
@@ -68,6 +68,22 @@ RunTest({
                })
 })
 
+RunTest({
+  acceptable_error <- 0.01
+  g1 <- g2 <- g3 <- list()
+  for (i in 1:20){
+    g1[[i]] <- igraph::sample_gnp(100, 0.50, directed = TRUE)
+    g2[[i]] <- igraph::sample_gnp(100, 0.50, directed = TRUE)
+    g3[[i]] <- igraph::sample_gnp(100, 0.32, directed = TRUE)
+  }
+  G <- c(g1, g2, g3)
+  label <- c(rep(1, 20), rep(2, 20), rep(3, 20))
+  result <- anogva(G, label, maxBoot=50, npoints=200)
+  test_that("Anogva with directed graphs.", {
+    expect_lt(result$p.value, acceptable_error)
+  })
+})
+
 
 # TODO: Should we also test H1? We would need to verify if the distribution of p-values obtained when all groups are the same form a uniform distribution.
 # This is probably a little bit too intense for automatic testing.
